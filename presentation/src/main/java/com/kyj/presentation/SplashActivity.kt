d38655d91636ad8dad5ff067877efb98c2e7c615
yeongjin <dudwls901@naver.com>
2022-11-14 18:21:46 +0900

    [Add] @AndroidEntryPoint 추가

--- presentation/src/main/java/com/kyj/presentation/SplashActivity.kt
+++ presentation/src/main/java/com/kyj/presentation/SplashActivity.kt
@@ -1,13 +1,18 @@
 package com.kyj.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+        splashViewModel.getCoronaCenters()
     }
 }
