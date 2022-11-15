46ee2487e29d060e4e7f17389dbd8929a574b893
yeongjin <dudwls901@naver.com>
2022-11-15 12:09:40 +0900

    [Feat] 스플래시 화면 구현

--- presentation/src/main/java/com/kyj/presentation/SplashActivity.kt
+++ presentation/src/main/java/com/kyj/presentation/SplashActivity.kt
@@ -1,18 +1,71 @@
 package com.kyj.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import com.kyj.presentation.databinding.ActivitySplashBinding
+import com.kyj.presentation.util.event.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
     private val splashViewModel: SplashViewModel by viewModels()
+    private var duration = 0
+    private val initProgress: Job by lazy {
+        lifecycleScope.launch {
+            startAnim(1600)
+        }
+    }
+    private val continueProgress: Job by lazy {
+        lifecycleScope.launch {
+            initProgress.cancel()
+            startAnim(2000)
+            navigateMainActivity()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        splashViewModel.getCoronaCenters()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        startDownload()
+        observeDatas()
     }
+
+    private fun observeDatas() {
+        splashViewModel.errorMessage.observe(this, EventObserver {
+            Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show()
+        })
+    }
+
+    private fun startDownload() {
+        initProgress.start()
+        lifecycleScope.launch {
+            if (splashViewModel.getAllCoronaCenters(10)) {
+                continueProgress.start()
+            }
+        }
+    }
+
+    private suspend fun startAnim(end: Int) {
+        while (duration <= end) {
+            binding.loadingBar.progress = duration / 20
+            delay(1L)
+            duration++
+        }
+    }
+
+    private fun navigateMainActivity() {
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+    }
+
 }
--- presentation/src/main/java/com/kyj/presentation/SplashViewModel.kt
+++ presentation/src/main/java/com/kyj/presentation/SplashViewModel.kt
@@ -1,19 +1,54 @@
 package com.kyj.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kyj.domain.usecase.GetCoronaCentersUseCase
+import com.kyj.domain.usecase.InsertCoronaCentersUseCase
+import com.kyj.domain.util.NetworkResult
+import com.kyj.domain.util.getErrorMessage
+import com.kyj.presentation.util.event.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import timber.log.Timber
+import kotlinx.coroutines.async
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val getCoronaCentersUseCase: GetCoronaCentersUseCase,
+    private val insertCoronaCentersUseCase: InsertCoronaCentersUseCase,
 ) : ViewModel() {
 
-    fun getCoronaCenters() = viewModelScope.launch {
-        Timber.d("${getCoronaCentersUseCase(1)}")
-    }
+    private var _errorMessage = MutableLiveData<Event<String>>()
+    val errorMessage: LiveData<Event<String>>
+        get() = _errorMessage
+
+    suspend fun getAllCoronaCenters(maxPage: Int) = viewModelScope.async {
+        var isSuccess = true
+        for (page in 1..maxPage) {
+            isSuccess = getEachPageCoronaCenters(page)
+        }
+        isSuccess
+    }.await()
+
+    private suspend fun getEachPageCoronaCenters(page: Int): Boolean = viewModelScope.async {
+        var isSuccess: Boolean
+        with(getCoronaCentersUseCase(page)) {
+            isSuccess = when (this) {
+                is NetworkResult.Success -> {
+                    insertCoronaCentersUseCase(this.value.data.toTypedArray())
+                    true
+                }
+                is NetworkResult.Fail -> {
+                    _errorMessage.value = Event(this.message)
+                    false
+                }
+                is NetworkResult.Exception -> {
+                    _errorMessage.value = Event(this.errorType.getErrorMessage())
+                    false
+                }
+            }
+        }
+        isSuccess
+    }.await()
 }
