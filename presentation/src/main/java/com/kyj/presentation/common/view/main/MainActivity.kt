e489301d19907c850c737905a04f36631a0a8d62
yeongjin <dudwls901@naver.com>
2022-11-16 11:59:48 +0900

    [Feat] 정보창 구현

--- presentation/src/main/java/com/kyj/presentation/common/view/main/MainActivity.kt
+++ presentation/src/main/java/com/kyj/presentation/common/view/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kyj.presentation.common.view.main
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -14,6 +15,7 @@ import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.overlay.OverlayImage
@@ -30,8 +32,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var naverMap: NaverMap
     private lateinit var mapFragment: MapFragment
+    private val infoWindow by lazy { InfoWindow() }
+    private val markerIcon: OverlayImage by lazy {
+        OverlayImage.fromResource(com.naver.maps.map.R.drawable.navermap_default_marker_icon_green)
+    }
+    private val overlayClickListener: Overlay.OnClickListener by lazy {
+        Overlay.OnClickListener { overlay ->
+            mainViewModel.updateSelectedCoronaCenter(overlay.tag as Int)
+            false
+        }
+    }
 
-    private val markerIcon: OverlayImage by lazy { OverlayImage.fromResource(com.naver.maps.map.R.drawable.navermap_default_marker_icon_green) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,25 +65,55 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun observeDatas() {
         mainViewModel.coronaCenters.observe(this) { coronaCenters ->
-            coronaCenters.forEach { coronaCenter ->
-                setMarkerInMap(coronaCenter)
+            lifecycleScope.launch {
+                coronaCenters.forEach { (_, coronaCenterUi) ->
+                    setMarkerInMap(coronaCenterUi)
+                }
             }
+        }
+        mainViewModel.selectedCoronaCenter.observe(this) { coronaCenterUi ->
+            val marker = coronaCenterUi.marker
+            moveCamera(marker.position)
+            showInfoWindow(
+                marker,
+                coronaCenterUi.coronaCenter
+            )
         }
     }
 
-    private fun setMarkerInMap(
+    private fun showInfoWindow(
+        marker: Marker,
         coronaCenter: CoronaCenter,
     ) {
-        lifecycleScope.launch {
-            val marker = withContext(Dispatchers.Default) {
-                setOverlays(coronaCenter)
-            }
-            marker.map = naverMap
+        infoWindow.adapter = InfoWindowAdapter(
+            this,
+            binding.root as ViewGroup,
+            coronaCenter
+        )
+        if (marker.infoWindow == null) {
+            infoWindow.open(marker)
+        } else {
+            infoWindow.close()
         }
     }
 
-    private fun setOverlays(coronaCenter: CoronaCenter): Marker {
-        return Marker().apply {
+    private suspend fun setMarkerInMap(
+        coronaCenterUi: CoronaCenterUi,
+    ) {
+        withContext(Dispatchers.Default) {
+            setOverlay(
+                coronaCenterUi.marker,
+                coronaCenterUi.coronaCenter
+            )
+        }
+        coronaCenterUi.marker.map = naverMap
+    }
+
+    private fun setOverlay(
+        marker: Marker,
+        coronaCenter: CoronaCenter,
+    ) {
+        with(marker) {
             position = LatLng(coronaCenter.lat, coronaCenter.lng)
             onClickListener = overlayClickListener
             tag = coronaCenter.id
@@ -81,20 +122,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private val overlayClickListener = Overlay.OnClickListener { overlay ->
-        moveCamera(overlay.tag as Int)
-        false
-    }
-
-    private fun moveCamera(id: Int) {
-        mainViewModel.getMarkerLatLng(id)?.let { (lat, lng) ->
-            val cameraUpdate = CameraUpdate.scrollTo(
-                LatLng(
-                    lat,
-                    lng
-                )
-            ).animate(CameraAnimation.Easing)
-            naverMap.moveCamera(cameraUpdate)
-        }
+    private fun moveCamera(latLng: LatLng) {
+        val cameraUpdate = CameraUpdate.scrollTo(
+            latLng
+        ).animate(CameraAnimation.Easing)
+        naverMap.moveCamera(cameraUpdate)
     }
 }
--- presentation/src/main/java/com/kyj/presentation/common/view/main/MainViewModel.kt
+++ presentation/src/main/java/com/kyj/presentation/common/view/main/MainViewModel.kt
@@ -1,25 +1,40 @@
 package com.kyj.presentation.common.view.main
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.kyj.domain.model.CoronaCenter
 import com.kyj.domain.usecase.GetCoronaCentersUseCase
+import com.naver.maps.map.overlay.Marker
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     getCoronaCentersUseCase: GetCoronaCentersUseCase,
 ) : ViewModel() {
-    val coronaCenters: LiveData<List<CoronaCenter>> = getCoronaCentersUseCase().asLiveData()
+    val coronaCenters: LiveData<Map<Int, CoronaCenterUi>> = getCoronaCentersUseCase().map { coronaCenters ->
+        coronaCenters.associate { coronaCenter ->
+            Pair(
+                coronaCenter.id,
+                parseCoronaCenterToCoronaCenterUi(coronaCenter)
+            )
+        }
+    }.asLiveData()
 
-    fun getMarkerLatLng(id: Int): Pair<Double, Double>? {
-        val foundedCoronaCenter = coronaCenters.value?.find { coronaCenter ->
-            coronaCenter.id == id
-        }
-        return foundedCoronaCenter?.let { coronaCenter ->
-            Pair(coronaCenter.lat, coronaCenter.lng)
-        }
+    private val _selectedCoronaCenter = MutableLiveData<CoronaCenterUi>()
+    val selectedCoronaCenter: LiveData<CoronaCenterUi>
+        get() = _selectedCoronaCenter
+
+    fun updateSelectedCoronaCenter(id: Int) {
+        _selectedCoronaCenter.value = coronaCenters.value?.get(id)
     }
+
+    private fun parseCoronaCenterToCoronaCenterUi(coronaCenter: CoronaCenter) = CoronaCenterUi(
+        Marker(),
+        coronaCenter
+    )
 }
